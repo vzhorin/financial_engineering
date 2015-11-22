2cc20dc2ec288adec00093de047ab0825385fbcd
Victor Zhorin <vzhorin@gmail.com>
2015-11-22 12:47:35 -0600

    add halton

--- halton.m
+++ halton.m
@@ -1,11 +1,10 @@
 % van der Corput generator for Halton sequences
-% b - prime number, n - interger number
-% h(n,b) = sum_{k=0}^L(d_k b^{-k-1})
-%for i=1:1000 a(i) = (halton(i,7)); end
-%for i=1:1000 b(i) = (halton(i,13)); end
-%figure;plot(rand(1000,1),rand(1000,1),'o')
+% b - base prime number, n - integer number recorded in a base b
+% h(n,b) = sum_{k=0}^L*[d_k* b^{-k-1}]
+%tic; for i=1:10000 a(i) = (halton(i,7)); end;for i=1:10000 b(i) = (halton(i,13)); end;figure; plot(a,b,'o');toc
+%tic;figure;plot(rand(10000,1),rand(10000,1),'o');toc;
 %figure; plot(a,b,'o')
-function h=halton(n,b)
+function h = halton(n,b)
 n_old = n;
 h = 0;
 f = 1/b;
